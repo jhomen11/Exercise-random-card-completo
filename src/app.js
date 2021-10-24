013a1e9bac56be6dadd0fa6f474b80a16abd775c
Jhonny Mendoza <jhonnymendoza@Mac-mini-de-Jhonny.local>
2021-10-24 16:12:05 -0300

    Se crea logica en JavaScript

--- src/app.js
+++ src/app.js
@@ -6,6 +6,37 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  cardGenerate();
+
+  setInterval(() => {
+    cardGenerate();
+  }, 5000);
 };
+
+const cardGenerate = () => {
+  let numRamdon = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+
+  let numPica = Math.floor(Math.random() * 4);
+  let picaRamdon = ["♦", "♥", "♠", "♣"];
+  let pica = picaRamdon[numPica];
+
+  let Caja1 = document.querySelector(".caja1");
+  Caja1.innerHTML = pica;
+  let Caja3 = document.querySelector(".caja3");
+  Caja3.innerHTML = pica;
+  let Caja2 = document.querySelector(".caja2");
+  Caja2.innerHTML = numRamdon;
+
+  if (pica == "♦" || pica == "♥") {
+    Caja1.style.color = "red";
+    Caja3.style.color = "red";
+    Caja2.style.color = "red";
+  } else {
+    Caja1.style.color = "black";
+    Caja3.style.color = "black";
+    Caja2.style.color = "black";
+  }
+};
+
+let boton = document.getElementById("btn");
+boton.addEventListener("click", cardGenerate);
